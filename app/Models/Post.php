c10eae16eb8016805e8f04c407c5a40202d10d53
dmechenko <dmechenko@gmail.com>
2021-11-14 14:56:53 -0500

    Sort posts by date

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,23 +20,29 @@ class Post
    * @param $body
    */
 
-  public function __construct($title, $excerpt, $body, $slug)
+  public function __construct($title, $excerpt, $date, $body, $slug)
   {
     $this->title = $title;
     $this->excerpt = $excerpt;
     $this->body = $body;
+    $this->date = $date;
     $this->slug = $slug;
   }
   
   public static function all(){
-    return collect(File::files(resource_path("posts")))
-      ->map(fn($file) => YamlFrontMatter::parseFile($file))
-      ->map(fn($document) => new Post(
-        $document->title,
-        $document->excerpt,
-        $document->body(),
-        $document->slug,
-      ));
+    return cache()->rememberForever('posts.all', function () {
+
+      return collect(File::files(resource_path("posts")))
+        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn($document) => new Post(
+          $document->title,
+          $document->excerpt,
+          $document->date,
+          $document->body(),
+          $document->slug,
+        ))
+        ->sortByDesc('date');
+    });
   }
 
   public static function find($slug){
