524ddfad5e8f804db546bff0e365cf0e1becc25f
dmechenko <dmechenko@gmail.com>
2021-11-16 15:49:44 -0500

    Add HTML and CSS

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+{{-- @extends('layout')
 
 @section('content')
   @foreach ($posts as $post)
@@ -16,4 +16,22 @@
     </div>
   </article>
   @endforeach
-@endsection
+@endsection --}}
+
+<x-layout>
+  @include('_posts-header')
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+      <x-post-featured-card />
+
+      <div class="lg:grid lg:grid-cols-2">
+        <x-post-card />
+        <x-post-card />
+      </div>
+
+      <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+        <x-post-card />
+        <x-post-card />
+      </div>
+  </main>
+</x-layout>
