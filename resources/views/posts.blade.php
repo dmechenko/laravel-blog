aaba0247b5d3c6ce61cd622f89dc16eed479a170
dmechenko <dmechenko@gmail.com>
2021-11-16 17:05:58 -0500

    More dynamic posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -21,17 +21,10 @@
 <x-layout>
   @include('_posts-header')
   <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-      <x-post-featured-card :post="$posts[0]"/>
-
-      <div class="lg:grid lg:grid-cols-2">
-        <x-post-card />
-        <x-post-card />
-      </div>
-
-      <div class="lg:grid lg:grid-cols-3">
-        <x-post-card />
-        <x-post-card />
-        <x-post-card />
-      </div>
+    @if ($posts->count())
+      <x-posts-grid :posts="$posts" />
+    @else
+      <p>No posts yet.</p>
+    @endif
   </main>
 </x-layout>
